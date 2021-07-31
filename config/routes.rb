5b048b5d5c04c840e9482226aa7600e8410bdd56
EC2 Default User <ec2-user@ip-172-31-18-49.ec2.internal>
2021-07-31 14:36:43 +0000

    [Add] ジャンル一覧画面まで作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :admins
+
+  devise_for :admin, controllers:{
+    sessions: 'admin/sessions'
+  }
+
+  namespace :admin do
+    resources :genres, only: [:index, :create, :edit, :update]
+  end
+
+#:books, only: [:destroy]
+
+
+
+
+
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
