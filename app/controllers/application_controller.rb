70ea552aa8ff85c403499522b373b3a62d0a584c
EC2 Default User <ec2-user@ip-172-31-18-49.ec2.internal>
2021-08-02 10:13:32 +0000

    [ADD] 管理者ページ作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,28 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin
+      admin_root_path       #pathは設定したい遷移先へのpathを指定してください
+    when Customer
+      root_path              #ここもpathはご自由に変更してください
+    end
+  end
+
+
+
+
+
+
+
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'public/homes#top'
+  get 'about' => 'public/homes#about'
+
+  devise_for :customers
+
+
 
   devise_for :admin, controllers:{
     sessions: 'admin/sessions'
@@ -7,16 +13,13 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :genres, only: [:index, :create, :edit, :update]
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
-    
+    resources :customers, only: [:index, :show, :edit, :update]
+    root to: 'homes#top'
+    resources :orders, only: [:show]
   end
 
 #:books, only: [:destroy]
 
-
-
-
-
-  devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
